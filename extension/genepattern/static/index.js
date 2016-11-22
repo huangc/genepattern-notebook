283ce40981a5ecfaf1759980d9fa79b42c2147b6
tmtabor <tabor@broadinstitute.org>
2016-11-21 16:19:08 -0800

    Test commit for new branch

--- extension/genepattern/static/index.js
+++ extension/genepattern/static/index.js
@@ -19,7 +19,7 @@ define([
     "nbextensions/genepattern/resources/navigation",
     "nbextensions/genepattern/resources/auth-widget",
     "nbextensions/genepattern/resources/job-widget",
-    "nbextensions/genepattern/resources/task-widget"], function(Jupyter, events) {
+    "nbextensions/genepattern/resources/task-widget"], function(Jupyter, events, $) {
 
     function load_ipython_extension() {
         var STATIC_PATH = Jupyter.notebook.base_url + "nbextensions/genepattern/resources/";
